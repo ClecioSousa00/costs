ae5dbca9ca73aa31981a94565b8523af6098bcc6
ClecioSousa00 <cleciosouza015@gmail.com>
2023-05-03 10:35:47 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './styles/elements/base.css'
+import './styles/generic/reset.css'
+import './styles/settings/colors.css'
+
+function App() {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
