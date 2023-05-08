4feb01073bc54ebb2226570c06b2c1d39c6bb82d
ClecioSousa00 <cleciosouza015@gmail.com>
2023-05-08 19:19:31 -0300

    remove class option form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -7,9 +7,9 @@ export const Form = () =>{
             <input className="form_input" type="text" placeholder="Nome do Projeto" />
             <input className="form_input" type="number" placeholder="Orçamento do Projeto" />
             <select className="form_select" name="category_id" id="">
-                <option className="teste">Selecione uma opcao</option>
-                <option className="teste">texto1</option>
-                <option className="teste">texto2</option>
+                <option>Selecione uma opcao</option>
+                <option>texto1</option>
+                <option>texto2</option>
             </select>
             <SubmitButton text="Criar Projeto"/>
         </form>
