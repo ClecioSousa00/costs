dcbf88e4be2d1762559aeb3fb2e16793da3d6703
ClecioSousa00 <cleciosouza015@gmail.com>
2023-05-10 10:34:26 -0300

    created the get and post method

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,16 +1,41 @@
+import { useState } from "react"
 import { SubmitButton } from "../SubmitButton"
 import "./Form.css"
 
-export const Form = () =>{
+export const Form = ({handleSubmit, projectData, categories}) =>{
+
+    const [project, setProject] = useState(projectData || {})
+
+    const submitForm = (event) =>{
+        event.preventDefault()
+        handleSubmit(project)
+        console.log(project);
+    }
+
+    const handleChange = (event) =>{
+        setProject({...project, [event.target.name]: event.target.value})
+    }
+
     return(
-        <form className="form" onSubmit={()=>{}}>
-            <input className="form_input" type="text" placeholder="Nome do Projeto" />
-            <input className="form_input" type="number" placeholder="Orçamento do Projeto" />
-            <select className="form_select" name="category_id" id="">
+        <form className="form" onSubmit={submitForm}>
+            <input 
+                className="form_input" 
+                type="text" 
+                name="nameproject" 
+                placeholder="Nome do Projeto"
+                onChange={handleChange}
+            />
+            <input 
+                className="form_input" 
+                type="number" 
+                name="budgetValue" 
+                placeholder="Orçamento do Projeto"
+                onChange={handleChange}
+            />
+            {/* <select className="form_select" name="category_id" id="">
                 <option>Selecione uma opcao</option>
-                <option>texto1</option>
-                <option>texto2</option>
-            </select>
+                {categories.map(categorie => <option key={categorie.id} value={categorie.id}>{categorie.name}</option>)}
+            </select> */}
             <SubmitButton text="Criar Projeto"/>
         </form>
     )
--- src/pages/NewProject/index.jsx
+++ src/pages/NewProject/index.jsx
@@ -1,7 +1,42 @@
 import { Header } from "../../components/Header"
-import {Form} from "../../components/Form"
+import { Form } from "../../components/Form"
+import { useEffect, useState } from "react"
 
 export const NewProject = () => {
+
+    const [categories, setCategories] =useState([])
+
+    useEffect(() => {
+        console.log('aquiGet')
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => response.json())
+            .then(data => setCategories(data))
+            .catch(err => console.log('Erro'))
+    }, [])
+
+    const createPost = (project) =>{
+        project.cost = 0
+        project.services = []
+
+        
+        fetch('http://localhost:5000/projects', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(project),
+        })
+            .then(response => response.json())
+            .then(data => setCategories(data))
+            .catch(err => console.log('Erro'))
+            console.log('aquiPost')
+    }
+
     return (
         <>
             <Header />
@@ -9,7 +44,7 @@ export const NewProject = () => {
                 <section className="section">
                     <h1 className="title">Criar Projetos</h1>
                     <p className="text">Crie seu projeto para adicionar serviços</p>
-                    <Form/>
+                    <Form handleSubmit={createPost}  />
                 </section>
             </main>
         </>
