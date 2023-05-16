7794c304b55150a812b3d8c82acfa62c6ff7e665
ClecioSousa00 <cleciosouza015@gmail.com>
2023-05-16 10:45:46 -0300

    created form new services

---
+++ src/components/ServiceForm/index.jsx
@@ -0,0 +1,50 @@
+
+import { useState } from "react"
+
+import { SubmitButton } from "../SubmitButton"
+
+
+
+export const ServiceForm = (handleSubmit, textBtn, projectData) => {
+
+    const [service, setService] = useState({})
+
+    const submitForm = (event) => {
+        event.preventDefault()
+        projectData.services.push(service)
+        handleSubmit(projectData)
+    }
+
+    const handleChange = (event) => {
+        setService({...service, [event.target.name]: event.target.value})
+    }
+
+    return (
+        <>
+            <form className="form" onSubmit={submitForm}>
+                <input
+                    className="form_input"
+                    type="text"
+                    name="nameService"
+                    placeholder="Nome do Serviço"
+                    onChange={handleChange}
+                />
+                <input
+                    className="form_input"
+                    type="number"
+                    name="costValue"
+                    placeholder="Custo do Serviço"
+                    onChange={handleChange}
+                />
+                <input
+                    className="form_input"
+                    type="text"
+                    name="descriptionService"
+                    placeholder="Descreva o Serviço"
+                    onChange={handleChange}
+                />
+                <SubmitButton text="Adicionar Serviço" />
+            </form>
+        </>
+    )
+}
--- src/pages/project/index.jsx
+++ src/pages/project/index.jsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+
 import { Form } from "../../components/Form"
+import { ServiceForm } from "../../components/ServiceForm"
+
+import "./Project.css"
 
 export const Project = () => {
 
     const { id } = useParams()
     const [project, setProject] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false)
 
     useEffect(() => {
         fetch(`http://localhost:5000/projects/${id}`, {
@@ -37,29 +42,42 @@ export const Project = () => {
             .catch(err => console.log("erro ao pegar projeto ao clicar em editar"))
     }
 
-
+    const createService = () =>{
+        
+    }
 
     const toggleProjectForm = () => {
         setShowProjectForm(!showProjectForm)
     }
-
-    if (!project) {
-        console.log('projeto ainda n existe');
-        return
+    const toggleServiceForm = () => {
+        setShowServiceForm(!showServiceForm)
     }
 
+    if (!project) {
+        console.log('projeto ainda n existe');//colocar loader
+        return
+    }
+    //fazer a div ser um componente
     return (
-        <main>
+        <main className="edit_project">
             <h1>Projeto: {project.nameproject}</h1>
-            <button onClick={toggleProjectForm}>{showProjectForm ? 'Fechar' : 'Editar Projeto'}</button>
+            <button className="edit_btn" onClick={toggleProjectForm}>{showProjectForm ? 'Fechar' : 'Editar Projeto'}</button>
             {!showProjectForm ?
-                <div>
+                <div className="description_project">
                     <p>Categoria: {project.categorie}</p>
                     <p>Orçamento: {project.budgetValue}</p>
                     <p>Valor Gasto: {project.cost}</p>
                 </div> :
                 <Form handleSubmit={editProject} projectData={project} textBtn="Salvar Alterações"/>
             }
+            <div className="service_project">
+                <h2>Adicione um serviço</h2>
+                <button className="edit_btn" onClick={toggleServiceForm}>{!showServiceForm ? 'Adicionar serviço' : 'Fechar'}</button>
+                <div>
+                    {showServiceForm && (<ServiceForm handleSubmit={createService} projectData={project}/>)}
+                </div>
+                <h2>Serviços</h2>
+            </div>
         </main>
     )
 }
