bbaef148bd804b104c041124efd8748847b85ee7
ClecioSousa00 <cleciosouza015@gmail.com>
2023-05-17 14:49:59 -0300

    add function close menu mobile

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -12,18 +12,18 @@ export const NavBar = () =>{
         setMenuIsOpen(prevValue => !prevValue)
     }
 
-    // const closeMenu = () =>{
-    //     setMenuIsOpen(false)
-    // }
+    const closeMenu = () =>{
+        setMenuIsOpen(false)
+    }
 
     return(
         <>
             <nav className={`${'nav_bar'} ${menuIsOpen && 'active'}`}>
                 <ul className="nav_bar_itens">
-                    <li><Link className='nav_bar_links' to="/">Home</Link></li>
-                    <li><Link className='nav_bar_links' to="/Projetos">Projetos</Link></li>
-                    <li><Link className='nav_bar_links' to="/Sobre">Sobre</Link></li>
-                    <li><Link className='nav_bar_links' to="/Contatos">Contato</Link></li>
+                    <li><Link onClick={() => closeMenu()} className='nav_bar_links' to="/">Home</Link></li>
+                    <li><Link onClick={() => closeMenu()} className='nav_bar_links' to="/Projetos">Projetos</Link></li>
+                    <li><Link onClick={() => closeMenu()} className='nav_bar_links' to="/Sobre">Sobre</Link></li>
+                    <li><Link onClick={() => closeMenu()} className='nav_bar_links' to="/Contatos">Contato</Link></li>
                 </ul>
             </nav>
             <MenuToogle isOpen={menuIsOpen} fnClickMenu={openMenu}/>
