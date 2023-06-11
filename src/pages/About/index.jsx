997332b3813cdbe7aa69032bad773dc3294bd47d
ClecioSousa00 <cleciosouza015@gmail.com>
2023-06-11 10:01:00 -0300

    feat: add about

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,7 +1,10 @@
+import './About.css'
+
 export const About =() =>{
     return(
-        <>
-            <h1>about</h1>
-        </>
+        <div className='about'>
+            <h1 className='title'>Sobre o Projeto</h1>
+            <p className='text'>Este projeto foi desenvolvido com o curso <a href="https://www.youtube.com/watch?v=FXqX7oof0I4&list=PLnDvRpP8BneyVA0SZ2okm-QBojomniQVO&ab_channel=MatheusBattisti-HoradeCodar">Hora de codar</a> e também com minhas alterações que ficou como desafio do curso implementar o modo responsivo.</p>
+        </div>
     )
 }
