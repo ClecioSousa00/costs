843779b62a893babcc919a0d1258714415a04a6a
ClecioSousa00 <cleciosouza015@gmail.com>
2023-06-12 14:03:56 -0300

    fix: json server

--- src/axios/axios.instance.js
+++ src/axios/axios.instance.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://json-test-nine.vercel.app/projects'
+  baseURL: 'http://localhost:5000/projects'
 })
 // baseURL: 'http://localhost:5000/projects'
+// "baseURL: 'https://json-test-nine.vercel.app/projects'"
--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -3,7 +3,6 @@ import { Cards } from "../../components/Cards"
 export const Contacts = () =>{
     return(
         <>
-            <h1>Contatos</h1>
             <Cards/>
         </>
     )
